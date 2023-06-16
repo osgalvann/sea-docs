7bf4ced6a610d8788f97f8f3e9f1703cd96b61f2
osgalvann <os.galvan.m@gmail.com>
2023-06-16 14:37:26 -0600

    refactoring

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
-export const sidebar = {
+export const docsSidebar = {
+  Introduction: [{ text: "What is SE Advantage?", link: "docs/intro" }],
   "bFO <-> SEA Integration": [
     { text: "Introduction", link: "test" },
     { text: "te", link: "test-2" },
     { text: "Page 3", link: "en/page-3" },
   ],
-  "Another Section": [{ text: "Page 4", link: "en/page-4" }],
 };
