16e28ea205b9d02f270c06574aed9d87372714a6
osgalvann <os.galvan.m@gmail.com>
2023-06-22 03:51:36 -0600

    added custom font for content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins"]
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
